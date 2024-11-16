283db42db303a57470258d1e30d1e820887e31c9
yui tamaki <fmsm-1414atok@docomo.ne.jp>
2024-11-16 07:34:13 +0000

    template

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("こんにちは")
+from flask import Flask,render_template
+app = Flask(__name__, static_folder='.', static_url_path='')
+@app.route('/',methods=['GET'])
+def hello():
+    return render_template('hello.html',title="grape",name="yui")
+
+
+    
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True,port=8888,threaded=True)
